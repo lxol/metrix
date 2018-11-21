50c96d51f1fe6faaeea0b35b2290b5e06aedd78d
Konrad Wudkowski <wudkowski@gmail.com>
2018-11-21 16:27:59 +0000

    PLATOPS-1783: Downgrade reactivemongo to 0.15.0 due to a regression introduced in 0.15.1

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +21,7 @@ object LibDependencies {
   def apply(): Seq[ModuleID] = compile ++ test
 
   private val play25Version = "2.5.12"
-  private val play26Version = "2.6.15"
+  private val play26Version = "2.6.20"
 
   private val compile: Seq[ModuleID] = PlayCrossCompilation.dependencies(
     shared = Seq(
@@ -30,14 +30,14 @@ object LibDependencies {
     play25 = Seq(
       "com.typesafe.play"     %% "play"                 % play25Version,
       "de.threedimensions"    %% "metrics-play"         % "2.5.13",
-      "uk.gov.hmrc"           %% "mongo-lock"           % "6.1.0-play-25",
-      "uk.gov.hmrc"           %% "simple-reactivemongo" % "7.0.0-play-25"
+      "uk.gov.hmrc"           %% "mongo-lock"           % "6.2.0-play-25",
+      "uk.gov.hmrc"           %% "simple-reactivemongo" % "7.4.0-play-25"
     ),
     play26 = Seq(
       "com.kenshoo"           %% "metrics-play"         % "2.6.6_0.6.2",
       "com.typesafe.play"     %% "play"                 % play26Version,
-      "uk.gov.hmrc"           %% "mongo-lock"           % "6.1.0-play-26",
-      "uk.gov.hmrc"           %% "simple-reactivemongo" % "7.0.0-play-26"
+      "uk.gov.hmrc"           %% "mongo-lock"           % "6.2.0-play-26",
+      "uk.gov.hmrc"           %% "simple-reactivemongo" % "7.4.0-play-26"
     )
   )
 
@@ -51,11 +51,11 @@ object LibDependencies {
     ),
     play25 = Seq(
       "com.typesafe.play" %% "play-test"          % play25Version   % Test,
-      "uk.gov.hmrc"       %% "reactivemongo-test" % "4.1.0-play-25" % Test
+      "uk.gov.hmrc"       %% "reactivemongo-test" % "4.2.0-play-25" % Test
     ),
     play26 = Seq(
       "com.typesafe.play" %% "play-test"          % play26Version   % Test,
-      "uk.gov.hmrc"       %% "reactivemongo-test" % "4.1.0-play-26" % Test
+      "uk.gov.hmrc"       %% "reactivemongo-test" % "4.2.0-play-26" % Test
     )
   )
 }
